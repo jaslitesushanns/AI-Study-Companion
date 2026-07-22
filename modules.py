55c2d80ea7fdee3aa310e0488460edc5aed795d3
jaslitesushanns <jaslitesushanns2026@gmail.com>
2026-07-22 09:41:55 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -7,8 +7,7 @@ import google.generativeai as genai
 # ---------------------------------
 def configure_gemini(api_key):
     genai.configure(api_key=api_key)
-    return genai.GenerativeModel("gemini-2.5-flash")
-
+    return genai.GenerativeModel("gemini-2.0-flash")
 
 # ---------------------------------
 # Study Plan Generator
