3ce394b90495d542290e2c963e676ede8b9b6d99
jaslite sushanns <jaslitesushanns2026@gmail.com>
2026-07-24 23:43:18 +0530

    Update modules.py

--- modules.py
+++ modules.py
@@ -6,7 +6,7 @@ api_key = os.getenv("GEMINI_API_KEY")
 
 if api_key:
     genai.configure(api_key=api_key)
-    model = genai.GenerativeModel("gemini-1.5-flash")
+    model = genai.GenerativeModel("gemini-2.5-flash")
 else:
     model = None
 # ---------------- COMMON FUNCTION ---------------- #
