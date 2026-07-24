a724922516a595d12d830ccd0dfdc0ef1bcf96c1
jaslite sushanns <jaslitesushanns2026@gmail.com>
2026-07-24 06:08:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,42 +1,43 @@
 import sqlite3
 
-DATABASE_NAME = "study_companion.db"
+DB_NAME = "study_companion.db"
+
 
 def get_connection():
-    conn = sqlite3.connect(DATABASE_NAME, check_same_thread=False)
+    conn = sqlite3.connect(DB_NAME, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
 
 
 def create_tables():
     conn = get_connection()
-    cursor = conn.cursor()
+    cur = conn.cursor()
 
-    # Users Table
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        email TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL,
+        email TEXT UNIQUE,
+        password TEXT,
         username TEXT,
         student_class TEXT,
         board TEXT,
         study_hours INTEGER,
         goal TEXT,
         weak_subjects TEXT,
+        xp INTEGER DEFAULT 0,
+        streak INTEGER DEFAULT 0,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
     """)
 
-    # Quiz Reports
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS reports (
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS reports(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER,
         subject TEXT,
         score INTEGER,
-        total_questions INTEGER,
-        report TEXT,
+        total INTEGER,
+        feedback TEXT,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
     """)
