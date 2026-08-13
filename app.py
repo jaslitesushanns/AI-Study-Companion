3a89a86d3d57b6e7f40605b20fa5e38cc7fb7f58
jaslite sushanns <jaslitesushanns2026@gmail.com>
2026-08-13 11:07:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -467,7 +467,7 @@ else:
                 st.markdown(timetable)
                         # ---------------- STUDY SESSION ---------------- #
 
-        elif menu == "📖 Study Session":
+    elif menu == "📖 Study Session":
 
             st.title("📖 AI Study Session Planner")
 
