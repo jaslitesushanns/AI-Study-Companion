aa60e897af02322519df17e2c5e11c68dff1425e
jaslite sushanns <jaslitesushanns2026@gmail.com>
2026-08-14 21:27:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -457,7 +457,7 @@ else:
             st.markdown(result)
         # ---------------- TIMETABLE ---------------- #
 
-elif menu == "🗓️ Timetable":
+    if menu == "🗓️ Timetable":
 
             st.title("🗓️ Smart Timetable")
 
