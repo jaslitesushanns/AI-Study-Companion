7a8483ea629b6b387b6298c750b7f3c2f2e38705
jaslite sushanns <jaslitesushanns2026@gmail.com>
2026-08-14 11:29:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ if not st.session_state.logged_in:
 
                 else:
 
-    if st.session_state.user is None:
+if st.session_state.user is None:
                 st.session_state.logged_in = False
                 st.rerun()
 
